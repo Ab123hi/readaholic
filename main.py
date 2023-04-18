d6eb87af324781e60beb081a2f729f7375d8f5af
Abhishek kumar <mabhishek741998@gmail.com>
2023-04-18 19:09:01 +0530

    login&register_route

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from forms import AdminRegistrationForm
+from forms import AdminLoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='secret_key_33'
 
 # website_name = "Readaholic"
 # data={
@@ -16,6 +19,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    form = AdminRegistrationForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template("register.html",form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = AdminLoginForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template("login.html",form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
